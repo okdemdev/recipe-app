02f272a23e3e1639f71171754c9009b3844eb4d3
damian dev <inamida16@gmail.com>
2024-06-12 22:20:59 +0300

    fetching and dynamic done.

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -1,18 +1,30 @@
 import { Heart, HeartPulse, Soup } from 'lucide-react';
 import React from 'react';
 
-const RecipeCard = () => {
+const getTwoValuesFromArray = (arr) => {
+  return [arr[0], arr[1]];
+};
+
+const RecipeCard = ({ recipe, bg, badge }) => {
+  const healthLabels = getTwoValuesFromArray(recipe.healthLabels);
+
   return (
     <>
-      <div className="flex flex-col rounded-md bg-[#ecf7d4] overflow-hidden p-3 relative ">
-        <a href="#" className="relative h-32">
+      <div
+        className={`flex flex-col rounded-md ${bg} overflow-hidden p-3 relative `}
+      >
+        <a
+          target="_blank"
+          href={`https://www.youtube.com/results?search_query=${recipe.label} recipe`}
+          className="relative h-32"
+        >
           <img
-            src="/1.jpg"
+            src={recipe.image}
             alt=""
             className="rounded-md w-full h-full object-cover cursor-pointer"
           />
           <div className="absolute bottom-2 left-2 bg-white rounded-full p-1 cursor-pointer flex items-center gap-1 text-sm">
-            <Soup /> 4 Servings
+            <Soup /> {recipe.yield} Servings
           </div>
           <div className="absolute top-1 right-2 bg-white rounded-full p-1 cursor-pointer">
             <Heart
@@ -23,22 +35,25 @@ const RecipeCard = () => {
         </a>
 
         <div className="flex mt-1">
-          <p className="font-bold tracking-wide">Roasted Chicken</p>
+          <p className="font-bold tracking-wide">{recipe.label}</p>
         </div>
-        <p className="my-2">Turkish Kitcen</p>
+        <p className="my-2">
+          {recipe.cuisineType[0].charAt(0).toUpperCase() +
+            recipe.cuisineType[0].slice(1)}{' '}
+          Kitchen
+        </p>
         <div className="flex gap-2 mt-auto">
-          <div className="flex gap-1 bg-[#d6f497] items-center p-1 rounded-md">
-            <HeartPulse size={16} />
-            <span className="text-sm tracking-tighter font-semibold">
-              Gluten-free
-            </span>
-          </div>
-          <div className="flex gap-1 bg-[#d6f497] items-center p-1 rounded-md">
-            <HeartPulse size={16} />
-            <span className="text-sm tracking-tighter font-semibold">
-              Heart-Healthy
-            </span>
-          </div>
+          {healthLabels.map((label, idx) => (
+            <div
+              key={idx}
+              className={`flex gap-1 ${badge} items-center p-1 rounded-md`}
+            >
+              <HeartPulse size={16} />
+              <span className="text-sm tracking-tighter font-semibold">
+                {label}
+              </span>
+            </div>
+          ))}
         </div>
       </div>
     </>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,36 @@
 import { Search } from 'lucide-react';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import RecipeCard from '../components/RecipeCard';
+import { getRandomColor } from '../lib/utils';
+
+const APP_ID = 'a6da9d27';
+const APP_KEY = '614ae49a918a4bb0d39185703e511712';
 
 const HomePage = () => {
+  const [recipes, setRecipes] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const fetchRecepies = async (searchQuery) => {
+    setLoading(true);
+    setRecipes([]);
+
+    try {
+      const res = await fetch(
+        `https://api.edamam.com/api/recipes/v2/?app_id=${APP_ID}&app_key=${APP_KEY}&q=${searchQuery}&type=public`
+      );
+      const data = await res.json();
+      setRecipes(data.hits);
+    } catch (error) {
+      console.log(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchRecepies('chicken');
+  }, []);
+
   return (
     <div className="bg-[#faf9fb] p-10 flex-1">
       <div className="max-w-screen-lg mx-auto">
@@ -24,13 +52,22 @@ const HomePage = () => {
         </p>
 
         <div className="grid gap-3 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-          <RecipeCard />
-          <RecipeCard />
-          <RecipeCard />
-          <RecipeCard />
-          <RecipeCard />
-          <RecipeCard />
-          <RecipeCard />
+          {!loading &&
+            recipes.map(({ recipe }, index) => (
+              <RecipeCard key={index} recipe={recipe} {...getRandomColor()} />
+            ))}
+
+          {loading &&
+            [...Array(9)].map((_, index) => (
+              <div key={index} className="flex flex-col gap-4 w-full">
+                <div className="skeleton h-32 w-full"></div>
+                <div className="flex justify-between">
+                  <div className="skeleton h-4 w-28"></div>
+                  <div className="skeleton h-4 w-24"></div>
+                </div>
+                <div className="skeleton h-4 w-1/2"></div>
+              </div>
+            ))}
         </div>
       </div>
     </div>
